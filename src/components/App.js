5a513e4b5f7a427af4858c9507d569723bf43c61
Anish Kumar <77631750+anishkumar127@users.noreply.github.com>
2023-07-30 11:36:28 +0000

    routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
 import React from "react";
 import './../styles/App.css';
+import Home from "./Home";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
